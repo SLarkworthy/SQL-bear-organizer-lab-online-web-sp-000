a592150cc742c3bdaf22ca7e333ec4c0ede9e93f
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-03-04 15:49:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,5 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabith
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 5, "male", "brown", "careful", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 8, "female", "brown", "bloodthirsty", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "male", "black", "sweetheart", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "male", "brown", "hungry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 10, "female", "tan", "cuddly", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "male", "brown", "hungry", 1);

059ab763a503bf90861d7698245013138bfe0079
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-03-04 15:46:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "male", "brown", "hungry", );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "male", "brown", "hungry", 1);
